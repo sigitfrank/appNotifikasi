3a6044154447b6bee6bbbb7d29f6fcef93d36249
sigitfrank <sigittt53@gmail.com>
2019-04-09 09:49:41 +0700

    appNotifikasi
    notifikasi menggunakan button, push notif dari firebase dengan token yang didapat ketika email didaftarkan, notif dikirim manual dari firebase cloud messaging

---
+++ pushNotif/app/src/main/java/i/sigit/pushnotif/User.java
@@ -0,0 +1,13 @@
+package i.sigit.pushnotif;
+
+public class User {
+
+    public String email;
+    public String token;
+
+    public User(String email, String token){
+        this.email = email;
+        this.token = token;
+    }
+
+}
